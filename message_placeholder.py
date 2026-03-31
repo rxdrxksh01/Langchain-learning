bfde87f08b970bb001230b097ad666a25dc61185
rxdrxksh01 <sharmarudraksh1001@gmail.com>
2026-03-31 15:13:58 +0530

    Implement structured chat history using HumanMessage and AIMessage for MessagesPlaceholder

--- message_placeholder.py
+++ message_placeholder.py
@@ -1,7 +1,20 @@
 from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
+from langchain_core.messages import HumanMessage, AIMessage
+
 chat_history=[]
-with open('chathistory.txt','r') as f:
-    chat_history.extend(f.readlines())
+# with open('chathistory.txt','r') as f:
+#     chat_history.extend(f.readlines())
+with open('chathistory.txt', 'r') as f:
+    for line in f:
+        line = line.strip()
+
+        if line.startswith("HumanMessage"):
+            content = line.split('content="')[1].split('"')[0]
+            chat_history.append(HumanMessage(content=content))
+
+        elif line.startswith("AIMessage"):
+            content = line.split('content="')[1].split('"')[0]
+            chat_history.append(AIMessage(content=content))
     
 
 
@@ -13,4 +26,4 @@ chat_template = ChatPromptTemplate([
 ])
 # print(chat_history)
 prompt = chat_template.invoke({'chat_history':chat_history,'query':'where is my refund'})
-# print(prompt)
+print(prompt)
