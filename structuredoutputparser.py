04c8cba503a463f36399137409c4bbdecc2d68c5
rxdrxksh01 <sharmarudraksh1001@gmail.com>
2026-04-09 11:13:08 +0530

    updated langchain import statements according to new docs

--- structuredoutputparser.py
+++ structuredoutputparser.py
@@ -1,7 +1,7 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
-from langchain.output_parsers import StructuredOutputParser, ResponseSchema
+from langchain_core.output_parsers import StructuredOutputParser, ResponseSchema
 
 load_dotenv()
 
